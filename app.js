aabdcd7f469b2e63aa7e8cc2dac253178f74d675
PaulusMaulusIII <paulschroeder05@outlook.de>
2023-11-16 20:29:16 +0100

    new

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const fs = require('fs');
 
-app.get("/", (req, res) => res.type('html').send("<html><body><h1>Moin</h1></body></html>"));
+app.get("/", (req, res) => res.type('text').send(fs.readFileSync("database.csv")));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
